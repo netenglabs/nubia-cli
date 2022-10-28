280f97b843380f6369443923d1a0d91b504d80e3
Claudio Lorina <claudio.lorina@gmail.com>
2022-10-28 12:34:07 +0200

    Fix first and last quote removed inside a quoted string
    Signed-off-by: Claudio Lorina <claudio.lorina@gmail.com>

--- nubia/internal/parser.py
+++ nubia/internal/parser.py
@@ -23,7 +23,9 @@ def _bool_transform(x):
 
 
 def _str_transform(x):
-    return x.strip("\"'")
+    if x and ((x[0] == x[-1] == '"') or (x[0] == x[-1] == '"')):
+        return x[1:-1]
+    return x
 
 
 _TRANSFORMS = {
